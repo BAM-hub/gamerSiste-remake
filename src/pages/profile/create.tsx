d303f16920aeb3e9bb5b432cca72b76cb9fe3264
BAM <bsharamin12@gmail.com>
2023-07-15 15:50:23 +0300

    test the context

--- src/pages/profile/create.tsx
+++ src/pages/profile/create.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Input from "~/components/shared/Input";
 import { api } from "~/utils/api";
 import { FaInstagram, FaFacebookF, FaTwitter } from "react-icons/fa6";
+import Form from "~/components/shared/Form";
 
 const Home: NextPage = () => {
   const { mutate: createProfile } = api.profile.createProfile.useMutation();
@@ -16,14 +17,37 @@ const Home: NextPage = () => {
       </Head>
       <main className="min-h-screen bg-slate-900">
         <h1 className="text-xl text-white">Create Profile</h1>
-        <form className="grid grid-cols-1 sm:grid-cols-2">
-          <Input label="Age" id="age" type="number" placeholder="Age" key={1} />
+
+        <Form
+          initialValues={{
+            general: { age: 0, username: "" },
+            // userName: "",
+            twitter: "",
+            facebook: "",
+            instagram: "",
+          }}
+          onSubmit={(event, values) => {
+            event.preventDefault();
+            console.log(event);
+            console.log(values);
+          }}
+          className="grid grid-cols-1 sm:grid-cols-2"
+        >
+          <Input
+            label="Age"
+            objectKey="age.age"
+            id="age"
+            type="number"
+            placeholder="Age"
+            key={1}
+          />
           <Input
             label="user name"
             id="user name"
             type="text"
             placeholder="user name"
             leftIcon={<span className="text-white">@</span>}
+            objectKey="userName"
             key={2}
           />
           <Input
@@ -31,12 +55,14 @@ const Home: NextPage = () => {
             id="twitter"
             type="text"
             placeholder="jhon doe"
+            objectKey="twitter"
             leftIcon={<FaTwitter className="text-white" />}
             key={3}
           />
           <Input
             label="facebook"
             id="facebook"
+            objectKey="facebook"
             type="text"
             placeholder="jhon doe"
             leftIcon={<FaFacebookF className="text-white" />}
@@ -45,13 +71,17 @@ const Home: NextPage = () => {
           <Input
             label="instagram"
             id="instagram"
+            objectKey="instagram"
             type="text"
             placeholder="jhon doe"
             leftIcon={<FaInstagram className="text-white" />}
             key={5}
           />
-        </form>
-        {/* <button
+          <button type="submit" className="bg-green-500">
+            submit
+          </button>
+        </Form>
+        {/* <button 
           onClick={() =>
             createProfile({
               preferedConsole: Console.PS4,
